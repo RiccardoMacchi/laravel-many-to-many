0ed521bba21a2e3a8b55febcde17023a871baf07
RiccardoMacchi <rick.mac97@outlook.com>
2024-09-25 19:16:47 +0200

    layout improved

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,10 +1,16 @@
 <aside class="text-bg-dark">
     <ul>
-        <li><a href="{{ route('admin.home') }}"><i class="fa-solid fa-house"></i> Home</a></li>
-        <li><a href="{{ route('admin.items.index') }}"><i class="fa-solid fa-list"></i> Elenco Lavori</a></li>
-        <li><a href="{{ route('admin.items.create') }}"><i class="fa-solid fa-newspaper"></i> Aggiungi Lavoro</a></li>
-        <li><a href="{{ route('admin.types.index') }}"><i class="fa-solid fa-layer-group"></i> Gestisci Tipi</a></li>
-        <li><a href="{{ route('admin.techs.index') }}"><i class="fa-solid fa-microchip"></i> Gestiscle Tecnologie</a>
+        <li><a href="{{ route('admin.home') }}"><i class="fa-solid fa-house"></i><span
+                    class="d-none d-md-inline">Home</span></a>
+        </li>
+        <li><a href="{{ route('admin.items.index') }}"><i class="fa-solid fa-list"></i><span class="d-none d-md-inline">
+                    Elenco Lavori</span></a></li>
+        <li><a href="{{ route('admin.items.create') }}"><i class="fa-solid fa-newspaper"></i><span
+                    class="d-none d-md-inline"> Aggiungi Lavoro</span></a></li>
+        <li><a href="{{ route('admin.types.index') }}"><i class="fa-solid fa-layer-group"></i><span
+                    class="d-none d-md-inline"> Gestisci Tipi</span></a></li>
+        <li><a href="{{ route('admin.techs.index') }}"><i class="fa-solid fa-microchip"></i><span
+                    class="d-none d-md-inline"> Gestiscle Tecnologie</span></a>
         </li>
     </ul>
 </aside>
